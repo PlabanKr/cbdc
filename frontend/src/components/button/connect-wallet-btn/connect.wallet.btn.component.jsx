8fe3222d6ee9f6b2dcfc655165f9e9da2d01f343
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2024-11-29 22:36:01 +0530

    Make the button into it's own component

---
+++ frontend/src/components/button/connect-wallet-btn/connect.wallet.btn.component.jsx
@@ -0,0 +1,11 @@
+import "./connect.wallet.btn.style.css";
+
+const ConnectWalletBtn = () => {
+  return (
+    <button className="connect-wallet-btn-main">
+      Connect Wallet
+    </button>
+  );
+};
+
+export default ConnectWalletBtn;
