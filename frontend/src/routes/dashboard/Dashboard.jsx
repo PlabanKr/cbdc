db65197c8d80b66a4bf0291876dc4668ae6e2141
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2024-11-29 22:45:06 +0530

    Change the buttons with the reusable components

--- frontend/src/routes/dashboard/Dashboard.jsx
+++ frontend/src/routes/dashboard/Dashboard.jsx
@@ -1,7 +1,9 @@
 import DashboardComponent from "../../components/main/DashboardComponent/dashboard.component";
 import Sidebar from "../../components/sidebar/sidebar.component";
-import { FaPowerOff } from "react-icons/fa6";
+// import { FaPowerOff } from "react-icons/fa6";
 import "./dashboard.css";
+import ConnectWalletBtn from "../../components/button/connect-wallet-btn/connect.wallet.btn.component";
+import LogoutBtn from "../../components/button/logout-btn/logout.btn.component";
 
 const Dashboard = () => {
   return (
@@ -9,8 +11,10 @@ const Dashboard = () => {
       <Sidebar />
       <div className="dashboard-main">
         <div className="dashboard-topbar">
-          <button className="dashboard-topbar-connect-wallet">Connect Wallet</button>
-          <button className="dashboard-topbar-logout-button"><FaPowerOff /></button>
+          {/* <button className="dashboard-topbar-connect-wallet">Connect Wallet</button> */}
+          <ConnectWalletBtn />
+          {/* <button className="dashboard-topbar-logout-button"><FaPowerOff /></button> */}
+          <LogoutBtn />
         </div>
         <div>
           <DashboardComponent />        
