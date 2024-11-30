c7e8aeae18ae3fab5a479750ec72a75c54dc5509
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2024-11-30 21:35:54 +0530

    Add new cards and fix positioning

--- frontend/src/components/main/BalanceComponent/balance.component.jsx
+++ frontend/src/components/main/BalanceComponent/balance.component.jsx
@@ -1,10 +1,14 @@
+import CryptoHoldings from "../../card/crypto-holdings/crypto.holdings.component";
+import InrBalance from "../../card/inr-balance/inr.balance.component";
 import TotalPortfolioValue from "../../card/total-portfolio-value/total-portfolio-value.component";
 import "./balance.style.css";
 
 const BalanceComponent = () => {
   return <div className="balance-component-main">
-    <div className="balance-component-info">
+    <div className="balance-component-info-cards">
       <TotalPortfolioValue />
+      <CryptoHoldings />
+      <InrBalance />
     </div>
   </div>
 }
